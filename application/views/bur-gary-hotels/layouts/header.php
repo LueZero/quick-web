a335168494573cad5928ebd9c03a7a4299cd3f75
zero <a0970937127@gmail.com>
2023-07-05 23:29:30 +0800

    bur-gary-hotels style.

--- application/views/bur-gary-hotels/layouts/header.php
+++ application/views/bur-gary-hotels/layouts/header.php
@@ -16,7 +16,7 @@
     <title>寶格利時尚旅館 - 西門町第一家精品時尚商務飯店</title>
     <link rel="shortcut icon" href="images/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo PUBLIC_PATH. '/bur-gary-hotel/scss/style.css'; ?>">
+    <link rel="stylesheet" href="<?php echo PUBLIC_PATH. '/bur-gary-hotel/css/style.css'; ?>">
 </head>
 
 <body>
